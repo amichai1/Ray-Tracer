98e76a71fed286dc02d8fb07c332c01b6200cf55
amichai1 <mukades@g.jct.ac.il>
2026-05-07 14:48:54 +0300

    refactor(renderer):   rework Camera and Scene (stage 5)

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -15,56 +15,67 @@ import primitives.Color;
  */
 public class Scene {
 
-   /**
-    * The name of the scene.
-    */
-   public final String name;
+    /**
+     * The name of the scene.
+     */
+    public final String name;
 
-   /**
-    * The background color rendered for rays that hit no geometry.
-    * Defaults to black.
-    */
-   public Color background = Color.BLACK;
+    /**
+     * The background color rendered for rays that hit no geometry.
+     * Defaults to black.
+     */
+    public Color background = Color.BLACK;
 
-   /**
-    * The ambient light applied uniformly to all surfaces in the scene.
-    * Defaults to no ambient light.
-    */
-   public AmbientLight ambientLight = AmbientLight.NONE;
+    /**
+     * The ambient light applied uniformly to all surfaces in the scene.
+     * Defaults to no ambient light.
+     */
+    public AmbientLight ambientLight = AmbientLight.NONE;
 
-   /**
-    * The collection of geometric bodies in the scene.
-    */
-   public final Geometries geometries = new Geometries();
+    /**
+     * The collection of geometric bodies in the scene.
+     */
+    public Geometries geometries = new Geometries();
 
-   /**
-    * Constructs a scene with the given name and default settings.
-    *
-    * @param  name the name of the scene
-    */
-   public Scene(String name) {
-      this.name = name;
-   }
+    /**
+     * Constructs a scene with the given name and default settings.
+     *
+     * @param name the name of the scene
+     */
+    public Scene(String name) {
+        this.name = name;
+    }
 
-   /**
-    * Sets the background color of the scene.
-    *
-    * @param  background the background color
-    * @return            this scene (for method chaining)
-    */
-   public Scene setBackground(Color background) {
-      this.background = background;
-      return this;
-   }
+    /**
+     * Sets the background color of the scene.
+     *
+     * @param background the background color
+     * @return this scene (for method chaining)
+     */
+    public Scene setBackground(Color background) {
+        this.background = background;
+        return this;
+    }
 
-   /**
-    * Sets the ambient light of the scene.
-    *
-    * @param  ambientLight the ambient light
-    * @return              this scene (for method chaining)
-    */
-   public Scene setAmbientLight(AmbientLight ambientLight) {
-      this.ambientLight = ambientLight;
-      return this;
-   }
+    /**
+     * Sets the ambient light of the scene.
+     *
+     * @param ambientLight the ambient light
+     * @return this scene (for method chaining)
+     */
+    public Scene setAmbientLight(AmbientLight ambientLight) {
+        this.ambientLight = ambientLight;
+        return this;
+    }
+
+    /**
+     * Sets the geometries of the scene.
+     *
+     * @param geometries the collection of geometries
+     * @return this scene (for method chaining)
+     */
+    public Scene setGeometries(Geometries geometries) {
+        this.geometries = geometries;
+        return this;
+    }
 }
